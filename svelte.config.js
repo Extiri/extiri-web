a769116e7b4c85ba380dc3173e4d0fc302e92aea
Wiktor Wójcik <wiktorwojcik112@gmail.com>
2022-12-27 09:17:20 +0100

    fix(Config): Fix Vercel not working.

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: vercel()
+		adapter: adapter()
 	}
 };
 
